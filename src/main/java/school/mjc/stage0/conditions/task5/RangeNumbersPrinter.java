6dfc21dd2c8ebf09e8a9e3d4f2575a4eabad8901
xden78 <venera.opt@gmail.com>
2024-10-19 21:50:29 +0300

    4

--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -5,14 +5,18 @@ public class RangeNumbersPrinter {
         // Create that will be printing if number is between 1 and 5("number is between 1 and 5"),
         // 6 and 10("number is between 6 and 10") or "out of range" using switch statement.
         switch (number) {
+            case (1):
             case (2):
             case (3):
             case (4):
+            case (5):
                 System.out.println("number is between 1 and 5");
                 break;
+            case (6):
             case (7):
             case (8):
             case (9):
+            case (10):
                 System.out.println("number is between 6 and 10");
                 break;
             default:
